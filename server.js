4ce4fc174b4f510585bdd684d013afd3e79a99ed
admin <philipp.christ.1997@gmail.com>
2021-01-29 18:38:02 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const FAVORITE_FOOD = process.env.FOOD;
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+  while(true) {
+    console.log(`My favorite food is ${FAVORITE_FOOD}!`);
+    await sleep(5000);
+  }
+}
+
+main();
